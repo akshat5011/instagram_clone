4f823f11e84336083543eb6e49315b6c3b07b05a
akshat5011 <akshatgarg2k4@gmail.com>
2025-02-25 19:49:13 +0530

    layout prop was depricated. Header-> Right side Pfp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,54 @@
 import Image from "next/image";
 import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+import { HomeModernIcon } from "@heroicons/react/24/solid";
 
 export default function Header() {
   return (
     <div className="flex items-center justify-between max-w-6xl">
+      {/* left */}
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image src="/Instone.png" layout="fill" className="object-contain" />
+        <Image
+          src="/Instone.png"
+          fill
+          alt="Instone"
+          className="object-contain"
+        />
       </div>
       <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
-        <Image src="/icon.png" layout="fill" className="object-contain" />
+        <Image
+          src="/icon.png"
+          fill
+          alt="Insto LOGO"
+          className="object-contain"
+        />
       </div>
 
+      {/* Middle */}
       <div className="relative ">
-        <div className="absolute top-1 left-2">
+        <div className="absolute top-2 left-2">
           <MagnifyingGlassIcon className="h-5 text-gray-500" />
         </div>
         <input
           type="text"
           placeholder="Search"
-          className="bg-gray-50 h-7 pl-10 px-1 border-gray-500 text-base focus:ring-black focus:border-black rounded-md"
+          className="bg-gray-50 h-7 pl-10 p-4 border-2 border-gray-500 text-base focus:ring-black focus:border-black rounded-md"
         />
       </div>
 
-      <h1>Right sides</h1>
+      {/* Right */}
+      <div className="flex space-x-4 items-center">
+        <HomeModernIcon className="hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+        <img
+          src="/defaultPfp.png"
+          alt="user-pfp"
+          className="h-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
