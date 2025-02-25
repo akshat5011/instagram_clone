60c0448164a4ba7c27439beb399968594a3cee98
akshat5011 <akshatgarg2k4@gmail.com>
2025-02-26 00:47:12 +0530

    style the story section + tailwindcss scrollbar + updated tailwindcss + additional settings

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { use, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import Story from "./Story";
@@ -7,18 +7,19 @@ export default function Stories() {
   const [storyUsers, setStoryUsers] = useState([]);
 
   useEffect(() => {
-    const storyUser = Array.from({ length: 20 }, (_, i)=> ({
-      username: faker.person.fullName(),
-      pfp: faker.image.avatar(),
+    const storyUser = Array.from({ length: 20 }, (_, i) => ({
+      username: faker.internet.username().toLowerCase(),
+      pfp: faker.image.personPortrait({ size: "512" }),
       id: i,
     }));
     setStoryUsers(storyUser);
   }, []);
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
       {storyUsers.map((users) => (
         <Story key={users.id} img={users.pfp} username={users.username} />
       ))}
     </div>
   );
 }
+// npm install --save-dev tailwind-scrollbar
